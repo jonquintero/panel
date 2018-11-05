fdcd415336a54f21f5ea498089db984543fc4943
Jonathan Quintero <jonquintero@hotmail.com>
2018-11-05 14:06:51 -0400

    change

--- tests/Feature/Admin/ListUsersTest.php
+++ tests/Feature/Admin/ListUsersTest.php
@@ -29,6 +29,51 @@ class ListUsersTest extends TestCase
     }
 
     /** @test */
+    function it_paginates_the_users()
+    {
+        factory(User::class)->times(12)->create([
+            'create_at' => now()->subDays(4),
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'Decimosexto Usuario',
+            'create_at' => now()->subDays(3),
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'Decimoseptimo Usuario',
+            'create_at' => now()->subDays(2),
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'Primer Usuario',
+            'create_at' => now()->subWeek(),
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'Segundo Usuario',
+            'create_at' => now()->subDays(5),
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'Tercer Usuario',
+            'create_at' => now()->subDays(6),
+        ]);
+
+
+
+        $this->get('/usuarios')
+            ->assertStatus(200)
+
+            ->assertSeeInOrder([
+                'Decimoseptimo Usuario',
+                'Decimosexto Usuario',
+                'Tercer Usuario'])
+            ->assertDontSee('Segundo Usuario')
+            ->assertDontSee('Primer Usuario');
+    }
+
+    /** @test */
     function it_shows_a_default_message_if_the_users_list_is_empty()
     {
         $this->get('/usuarios')
