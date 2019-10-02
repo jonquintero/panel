80c5363951cbd13c2681e9f27eec98e97b0a28dd
jonquintero <jonquintero@hotmail.com>
2019-10-02 16:00:32 -0400

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,9 +22,7 @@
 <header>
     <!-- Fixed navbar -->
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-logo" href="/">
-            <img src="/images/styde.png" alt="Styde" width="100" height="30">
-        </a>
+       
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
